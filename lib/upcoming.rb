c86a74fcb3b0490b2bef03d45cb4f1753e95c900
Dennis Martinez <dennis@dennmart.com>
2012-06-30 11:20:15 -0700

    Revert "Removing API keys from classes and setting them in settings file"
    This reverts commit 9fc869281bfde7675f3f9d3705d4d653977fa971.

--- lib/upcoming.rb
+++ lib/upcoming.rb
@@ -1,11 +1,15 @@
 # encoding: utf-8
 class Upcoming
+
+  API_KEY = "ef81d1a9f2"
+  ENDPOINT = "http://upcoming.yahooapis.com/services/rest/"
+
   def query(params = {})
     InstadateMobile::Logger.info "Querying Upcoming API"
     check_parameters(params)
     upcoming_params = build_params(params)
     InstadateMobile::Logger.debug "Upcoming parameters: #{params.inspect}"
-    response = RestClient.get(settings["endpoint"], :params => params)
+    response = RestClient.get(ENDPOINT, :params => params)
     return build_events(response)
   rescue RestClient::ResourceNotFound => e
     InstadateMobile::Logger.error "Upcoming API error: #{e.response}"
@@ -43,13 +47,13 @@ class Upcoming
     params[:max_date] = Date.parse(params[:date].to_s).strftime("%Y-%m-%d")
     params.delete(:date)
 
-    params.merge!({:api_key => settings["api_key"], :method => "event.search", :format => "json", :flags => "I"})
+    params.merge!({:api_key => API_KEY, :method => "event.search", :format => "json", :flags => "I"})
     return params
   end
 
   def get_categories
     @categories ||= begin
-      response = RestClient.get(settings["endpoint"], :params => { :api_key => settings["api_key"], :method => "category.getList", :format => "json" })
+      response = RestClient.get(ENDPOINT, :params => { :api_key => API_KEY, :method => "category.getList", :format => "json" })
       categories = JSON.parse(response)["rsp"]["category"]
       categories
     end
@@ -81,12 +85,5 @@ class Upcoming
     end
     
     return activities
-  end
-
-  def settings
-    @settings ||= begin
-                    settings_file = File.join(File.dirname(__FILE__), '..', 'settings.yml')
-                    YAML::load(File.open(settings_file))["upcoming"]
-                  end
   end
 end
--- lib/yelp.rb
+++ lib/yelp.rb
@@ -1,5 +1,11 @@
 # encoding: utf-8
 class Yelp
+  OAUTH_CONSUMER_KEY = "xmQv-J8g98h40EjnUFkmIQ"
+  OAUTH_CONSUMER_SECRET = "d0ZDmkuCGKWSSiS5-Fy99X7nn2c"
+  OAUTH_ACCESS_TOKEN = "QzuSzvPavIuzkg2NqYfKSb3RMROfFDU9"
+  OAUTH_ACCESS_SECRET = "h1QIIth-Qs2VCNGKlqpLjxKRqgU"
+  ENDPOINT = "http://api.yelp.com/"
+  RATING_THRESHOLD = 3.5
   REQUIRED_PARAMETERS = [:image_url,:address,:city]
 
   def query(params = {})
@@ -8,8 +14,8 @@ class Yelp
     yelp_params = build_params(params)
     InstadateMobile::Logger.debug "Yelp Parameters: #{yelp_params.inspect}"
     
-    consumer = OAuth::Consumer.new(settings["consumer_key"], settings["consumer_secret"], { :site => settings["endpoint"] })
-    access_token = OAuth::AccessToken.new(consumer, settings["access_token"], settings["access_secret"])
+    consumer = OAuth::Consumer.new(OAUTH_CONSUMER_KEY, OAUTH_CONSUMER_SECRET, { :site => ENDPOINT })
+    access_token = OAuth::AccessToken.new(consumer, OAUTH_ACCESS_TOKEN, OAUTH_ACCESS_SECRET)
     yelp_params_as_query = URI.escape(yelp_params.collect{|k,v| "#{k}=#{v}"}.join('&'))
     response = access_token.get("/v2/search?#{yelp_params_as_query}")
     InstadateMobile::Logger.debug "Yelp Response: #{response.body}"
@@ -52,7 +58,7 @@ class Yelp
     activities = []
     businesses = JSON.parse(response.body)["businesses"]
     businesses.each do |business|
-      next if business["rating"] < settings["ratings_threshold"]
+      next if business["rating"] < RATING_THRESHOLD
       activity_info = { :latitude => business["location"]["coordinate"]["latitude"], :longitude => business["location"]["coordinate"]["longitude"],
                         :rating => business["rating"], :source_category => business["categories"].map{ |cat| cat.first }, :name => business["name"],
                         :source_venue_id => business["id"], :image_url => business["image_url"], :business_url => business["mobile_url"], :phone => business["display_phone"], :address => business["location"]["address"][0], :city => business["location"]["city"] }
@@ -76,12 +82,5 @@ class Yelp
       end
     end
     return false
-  end
-
-  def settings
-    @settings ||= begin
-                    settings_file = File.join(File.dirname(__FILE__), '..', 'settings.yml')
-                    YAML::load(File.open(settings_file))["yelp"]
-                  end
   end
 end
